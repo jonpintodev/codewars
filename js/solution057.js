bec331ee804fb723dbfb287c380e17d41f3f3ca5
Jon Pinto <JonPinto@protonmail.com>
2022-07-08 14:05:19 -0400

    solution057 completed

---
+++ js/solution057.js
@@ -0,0 +1,15 @@
+// Write a function which converts the input string to uppercase.
+
+//P: a string
+//R: the string in all caps
+//E:
+//P:
+
+function makeUpperCase(str) {
+  //use the toUpperCase() method
+  return str.toUpperCase()
+}
+
+console.log(makeUpperCase('Hello'))//'HELLO'
+console.log(makeUpperCase('yes'))//'YES'
+console.log(makeUpperCase('stop yelling'))//'STOP YELLING'
