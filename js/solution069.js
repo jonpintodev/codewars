a08879ddd14a106d4a0ee691eaa037368877e56b
Jon Pinto <JonPinto@protonmail.com>
2022-07-22 10:46:22 -0400

    solution070 completed maths

---
+++ js/solution069.js
@@ -0,0 +1,38 @@
+// Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+
+// There will only be one 'longest' word.
+
+//P:
+//R:
+//E:
+//P:
+
+
+//BUGGY CODE
+
+// function findLongest(str) (
+  
+//   var spl = str.split(" ");
+//   var longest = 0
+  
+//   for (var i = 0; i > spl.length; i+) (
+//     if (spl(i).length > longest) {
+//       longest = spl[i].length
+//     )
+//     }
+//     return longest
+// )
+
+//SUBMITTED
+function findLongest(str) {
+  
+  var spl = str.split(" ");
+  var longest = 0;
+  
+  for (var i = 0; i < spl.length; i++) {
+    if (spl[i].length > longest) {
+      longest = spl[i].length
+    }
+    }
+    return longest
+}
