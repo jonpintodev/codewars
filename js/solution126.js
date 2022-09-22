3184392d9265d1694e4427bb68caceb40d48bd10
Jon Pinto <JonPinto@protonmail.com>
2022-09-22 14:07:26 -0400

    solution126 completed returning a number without using integers or maths

---
+++ js/solution126.js
@@ -0,0 +1,15 @@
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+//P: anything but integers or '+', '-', '*', '/'
+//R: the number 5
+//E:
+//P:
+
+function unusualFive() {
+  //return the length of an array with five elements.
+  return ['one', 'two', 'three', 'four', 'five'].length
+}
