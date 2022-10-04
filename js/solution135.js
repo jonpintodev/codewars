e0792c1c802b1aca185a2a438a8bf584c0e800ca
Jon Pinto <JonPinto@protonmail.com>
2022-10-04 17:22:44 -0400

    solution135 completed logalrithm Math.log HARDMATHS

---
+++ js/solution135.js
@@ -0,0 +1,11 @@
+// Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
+
+//P: a base number (x) and two integers (a , b)
+//R: 
+//E:
+//P:
+
+function logs(x , a, b){
+  return Math.log(a * b) / Math.log(x)
+}
+
