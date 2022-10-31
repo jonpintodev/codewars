3a3e05543d2e7c864022a962bf881c6fd4f9a139
Jon Pinto <JonPinto@protonmail.com>
2022-10-31 16:13:57 -0400

    solution153 completed .fromCharCode()

---
+++ js/solution153.js
@@ -0,0 +1,11 @@
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// get_char(65)
+// should return:'A'
+
+//a num
+//the corresponding ASCII character for that value
+
+getChar =c => String.fromCharCode(c)
