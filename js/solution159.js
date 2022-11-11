874efba15690cd46bd22713ae7755dce863bee02
Jon Pinto <JonPinto@protonmail.com>
2022-11-11 15:14:22 -0500

    solution159 completed parseInt()

---
+++ js/solution159.js
@@ -0,0 +1,11 @@
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+//a hex number as a string
+//convert to decimal number
+
+// function hexToDec(hexString){
+//   return parseInt(hexString, 16)
+// }
+
+
+hexToDec = hexString => parseInt(hexString, 16)
