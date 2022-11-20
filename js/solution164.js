eb6ee39c0653f138265a86d2c2850986084735b1
Jon Pinto <JonPinto@protonmail.com>
2022-11-19 21:03:49 -0500

    solution164 completed parseInt()

---
+++ js/solution164.js
@@ -0,0 +1,8 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+//a binary num as a str
+//a decimal number
+
+function binToDec(bin){
+  return parseInt(bin, 2);
+}
