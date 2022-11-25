7af0f50cefb9e5fa78fc82a5909be78e0ec5d00e
Jon Pinto <JonPinto@protonmail.com>
2022-11-25 15:28:44 -0500

    solution170 completed .charCodeAt()

---
+++ js/solution170.js
@@ -0,0 +1,14 @@
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+//a letter
+//the position on the alphabet
+
+// function position(letter){
+//   `Position of alphabet: ${letter.charCodeAt() - 96}`
+// }
+
+const position = letter => `Position of alphabet: ${letter.charCodeAt() - 96}`
