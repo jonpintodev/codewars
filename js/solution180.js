f4fbf01e54951cc5e11fc72c5604432ae480e84d
Jon Pinto <JonPinto@protonmail.com>
2022-12-11 18:42:26 -0500

    solution180 completed debugging function

---
+++ js/solution180.js
@@ -0,0 +1,16 @@
+// Grasshopper - Function syntax debugging
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+//a function with syntax errors
+//debugged function
+
+// function main [verb, noun]
+//   return verb + noun
+// }
+
+function main(verb, noun){
+  return verb + noun
+}
+
+console.log(main('run', 'dog'), 'rundog')
+console.log(main('take ', 'item'), 'take item')
