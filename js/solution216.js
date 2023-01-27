3743176363a05b64f19e26daf671aa3083933a2f
Jon Pinto <JonPinto@protonmail.com>
2023-01-26 19:33:22 -0500

    solution216 completed variable assignment debugging

---
+++ js/solution216.js
@@ -0,0 +1,12 @@
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+// var a == "dev"
+// var b == "Lab"
+
+// var name == a + b
+
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
