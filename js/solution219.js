3d5c3c762b527598785829314f5835a386426665
Jon Pinto <JonPinto@protonmail.com>
2023-01-29 17:56:46 -0500

    solution219 completed array method .join() also solution using loop

---
+++ js/solution219.js
@@ -0,0 +1,39 @@
+// Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+// Example:
+
+// input:
+//    [[ 0, 1, 2, 3, 4 ],
+//     [ 10,11,12,13,14 ],
+//     [ 20,21,22,23,24 ],
+//     [ 30,31,32,33,34 ]] 
+    
+// output:
+//      '0,1,2,3,4\n'
+//     +'10,11,12,13,14\n'
+//     +'20,21,22,23,24\n'
+//     +'30,31,32,33,34'
+// Array's length > 2.
+
+// function toCsvText(array) {
+//   //create a variable that is an empty string
+//   let result = ''
+//   //loop that iterates through the array
+//   for (let i = 0; i < array.length; i++) {
+//     result += array[i]
+//     if (i != array.length - 1){
+//       result += '\n'
+//     }
+//   }
+//   return result
+// }
+
+const toCsvText = array => array.join('\n')
+
+console.log(toCsvText(
+  [
+    [ 0, 1, 2, 3, 4 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]
+  ] ))
